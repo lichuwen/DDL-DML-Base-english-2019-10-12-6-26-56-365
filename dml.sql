375797107fbf5a06478cafa84e2f28dc09a4e310
lichuwen <37268385+lichuwen@users.noreply.github.com>
2020-06-26 23:12:57 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,16 @@
 -- Insert record
+insert into tbname(fie1,fie2,...) values(val1,val2,...);
+insert into student(id,name,age,sex) values('001','zhangsan',18,'male'); --sample
 
 -- Revise record
+update 表名称 set 列名称 = 新值 where 列名称 = 某值;
+update student set age=0 where age=18; --sample
 
---  Delete record
+-- Delete record
+delete from tbname where condition;
+delete from student where name='zhangsan'; --sample
 
 -- Search record
+select 列名称 from 表名称;
+select * from student; --sample
+select age from student; --sample
